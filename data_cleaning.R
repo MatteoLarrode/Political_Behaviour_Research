f194e8cf90a47376203ab475fc9ab2ffba95ebef
Matteo Larrode <matteo.larrode@gmail.com>
2023-12-07 01:23:10 +0000

    extracted variables of interest waves 1 & 2

--- data_cleaning.R
+++ data_cleaning.R
@@ -13,11 +13,72 @@ for (file in csv_files){
 }
 
 # ---- Aggregation of waves ----
-# Keep variables of interest in waves 2, 3, 4
-# Wave 2 only repeats Wave 1 so ignore for now
+# Wave 1 - Variables of interest
+# ID: UID_fes1
+# State of democracy: fes1_EE22_Q06
+# Personal duty to fight climate change: fes1_EE22_Q13B
+# Redistribution: fes1_EE22_Q14_1
+# Immigraition as cultural enriching: fes1_EE22_Q14_2
+# Death penalty: fes1_EE22_Q14_4
+# Islam as threat for French identity: fes1_EE22_Q14_5
+# Age (decade): cal_AGE
+# Diploma: cal_DIPL
+# Sex: cal_SEXE
+
+w1 <- 
+  wave1 |> 
+  select(
+    id = UID_fes1,
+    state_of_democracy = fes1_EE22_Q06,
+    personal_duty_to_fight_climate_change = fes1_EE22_Q13B,
+    redistribution = fes1_EE22_Q14_1,
+    immigration_as_cultural_enriching = fes1_EE22_Q14_2,
+    death_penalty = fes1_EE22_Q14_4,
+    islam_as_threat_for_french_identity = fes1_EE22_Q14_5,
+    age_decade = cal_AGE,
+    diploma = cal_DIPL,
+    sex = cal_SEXE
+  )
 
 # Wave 2 - Variables of interest
+# ID: UID_fes2
+# Secularism threatened: fes2_EE22_Q08_7
+# State of democracy: fes2_EE22_Q06
+# Support anti-health pass movement: fes2_EE22_Q16
+# Replacement of population by immigrants: fes2_EE22_Q17
+# Age (decade): cal_AGE
+# Diploma: cal_DIPL
+# Sex: cal_SEXE
+# Interest in politics: eayy_i1
+# Self positioning - left-right scale: eayy_i8
 
 w2 <- 
   wave2 |> 
-  select
+  select(
+    id = UID_fes2,
+    secularism_threatened = fes2_EE22_Q08_7,
+    state_of_democracy2 = fes2_EE22_Q06,
+    support_anti_health_pass_movement = fes2_EE22_Q16,
+    replacement_of_population_by_immigrants = fes2_EE22_Q17,
+    age_decade2 = cal_AGE,
+    diploma2 = cal_DIPL,
+    sex2 = cal_SEXE,
+    interest_in_politics = eayy_i1,
+    self_positioning_left_right = eayy_i8
+  )
+
+# Wave 3 - Variables of interest
+# ID: UID_fes2
+# Secularism threatened: fes2_EE22_Q08_7
+# State of democracy: fes2_EE22_Q06
+# Support anti-health pass movement: fes2_EE22_Q16
+# Replacement of population by immigrants: fes2_EE22_Q17
+# Age (decade): cal_AGE
+# Diploma: cal_DIPL
+# Sex: cal_SEXE
+# Interest in politics: eayy_i1
+# Self positioning - left-right scale: eayy_i8
+
+aggreg_waves <-
+  w2 |> 
+  left_join(w1)
